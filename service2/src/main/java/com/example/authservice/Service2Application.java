4d554ef1d4c3c9d1fd6366a67ccd00f713754808
Laamiri Fouzia <laamiri.f.fouzia@gmail.com>
2024-01-10 09:53:45 +0100

    test desktop

---
+++ service2/src/main/java/com/example/authservice/Service2Application.java
@@ -0,0 +1,15 @@
+package com.example.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Service2Application.class, args);
+		System.out.println("hello fouzia from service 2!!");
+	}
+
+}
